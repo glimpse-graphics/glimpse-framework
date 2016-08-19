ff59ac46f8b1403561e4ca931c237dfed50caa13
Sławomir Czerwiński <slawomir@czerwinski.info.pl>
2016-08-19 09:57:31 +0200

    Tests of square matrix converted to KotlinTest

---
+++ api/src/test/kotlin/glimpse/api/SquareMatrixSpec.kt
@@ -0,0 +1,38 @@
+package glimpse.api
+
+import io.kotlintest.specs.WordSpec
+
+class SquareMatrixSpec : WordSpec() {
+
+	init {
+
+		"Determinant of a square matrix" should {
+			"be 1 for identity" {
+				forAll((1..8).toList()) { size ->
+					SquareMatrix.identity(size).det == 1f
+				}
+			}
+			"be equal to product of all numbers for a diagonal matrix" {
+				forAll((1..8).toList()) { size ->
+					SquareMatrix(size) { row, col -> if (row == col) (row + 1).toFloat() else 0f }.det ==
+							(1..size).reduce { product, next -> product * next }.toFloat()
+				}
+			}
+			"be 0 for a 'row + col + 1' matrix" {
+				forAll((1..8).toList()) { size ->
+					SquareMatrix(size) { row, col -> (row + col + 1).toFloat() }.det == 0f
+				}
+			}
+		}
+
+		"List representation of a square matrix" should {
+			"contain all the elements of the matrix" {
+				forAll((1..8).toList()) { size ->
+					SquareMatrix(size) { row, col -> (row + col * size + 1).toFloat() }.asList() == (1..size).toList()
+				}
+			}
+		}
+
+	}
+
+}
