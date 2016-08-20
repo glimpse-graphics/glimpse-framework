64859331e4e56137def9e41fc6b95e0566bab361
Sławomir Czerwiński <slawomir@czerwinski.info.pl>
2016-08-20 20:59:25 +0200

    TextureCoordinates.toString()

--- api/src/main/kotlin/glimpse/api/TextureCoordinates.kt
+++ api/src/main/kotlin/glimpse/api/TextureCoordinates.kt
@@ -9,6 +9,11 @@ package glimpse.api
 data class TextureCoordinates(val u: Float, val v: Float) {
 
 	internal val _2f: Array<Float> by lazy { arrayOf(u, v) }
+
+	/**
+	 * Returns a string representation of the [TextureCoordinates].
+	 */
+	override fun toString() = "(%.1f, %.1f)".format(u, v)
 }
 
 /**
--- api/src/test/kotlin/glimpse/api/TextureCoordinatesSpec.kt
+++ api/src/test/kotlin/glimpse/api/TextureCoordinatesSpec.kt
@@ -6,6 +6,12 @@ class TextureCoordinatesSpec : WordSpec() {
 
 	init {
 
+		"String representation of texture coordinates" should {
+			"be properly formatted" {
+				TextureCoordinates(0.3f, 0.7f).toString() shouldBe "(0.3, 0.7)"
+			}
+		}
+
 		"Direct buffer of texture coordinates" should {
 			val buffer = listOf(TextureCoordinates(0f, 1f), TextureCoordinates(1f, 1f), TextureCoordinates(1f, 0f)).toDirectBuffer()
 			"be direct" {
