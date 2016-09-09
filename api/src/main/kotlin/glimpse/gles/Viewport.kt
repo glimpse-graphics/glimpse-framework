d2ee2e2b41ed5dee21516b54ae420db677d14d47
Sławomir Czerwiński <slawomir@czerwinski.info.pl>
2016-09-09 23:28:06 +0200

    Added aspect ratio of viewport

--- api/src/main/kotlin/glimpse/gles/Viewport.kt
+++ api/src/main/kotlin/glimpse/gles/Viewport.kt
@@ -9,6 +9,11 @@ package glimpse.gles
 data class Viewport(val width: Int, val height: Int) {
 
 	/**
+	 * Viewport aspect ratio.
+	 */
+	val aspect: Float by lazy { width.toFloat() / height.toFloat() }
+
+	/**
 	 * Returns a string representation of the [Viewport].
 	 */
 	override fun toString(): String = "$width×$height"
