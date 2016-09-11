1aa9ff6e2fb19500b21aff6fad71ee6d23c9b5dc
Sławomir Czerwiński <slawomir@czerwinski.info.pl>
2016-09-11 18:04:11 +0200

    Added tests for orthographic matrix

--- api/src/test/kotlin/glimpse/ProjectionMatrixSpec.kt
+++ api/src/test/kotlin/glimpse/ProjectionMatrixSpec.kt
@@ -8,10 +8,14 @@ class ProjectionMatrixSpec : GlimpseSpec() {
 
 		"Perspective projection" should {
 			"be consistent with frustum projection" {
-				println(perspectiveProjectionMatrix(90.degrees, 1f, 1f, 2f))
-				println(frustumProjectionMatrix(-1f, 1f, -1f, 1f, 1f, 2f))
 				perspectiveProjectionMatrix(90.degrees, 1f, 1f, 2f) shouldBeRoughly
 						frustumProjectionMatrix(-1f, 1f, -1f, 1f, 1f, 2f)
+			}
+		}
+
+		"Orthographic projection" should {
+			"be consistent with identity matrix" {
+				orthographicProjectionMatrix(-1f, 1f, -1f, 1f, -1f, 1f) shouldBeRoughly Matrix.IDENTITY
 			}
 		}
 
