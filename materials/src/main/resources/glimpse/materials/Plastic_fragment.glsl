0b608e0b294fe4fc34afdd712075e405e05b6bc1
Sławomir Czerwiński <slawomir@czerwinski.info.pl>
2016-09-13 20:33:06 +0200

    Fixed plastic material (and shaders)

--- materials/src/main/resources/glimpse/materials/Plastic_fragment.glsl
+++ materials/src/main/resources/glimpse/materials/Plastic_fragment.glsl
@@ -5,29 +5,33 @@ uniform vec4 u_SpecularColor;
 uniform float u_Shininess;
 
 uniform vec4 u_LightDirection;
-uniform vec4 u_CameraPosition;
 
-varying vec4 v_VertexPosition;
+varying vec3 v_VertexPosition;
 varying vec4 v_VertexNormal;
-varying mat4 v_MVMatrix;
+varying vec2 v_TextureCoordinates;
+
+varying mat4 v_LightMatrix;
 varying mat4 v_NormalMatrix;
+
+float positiveDot(vec3 a, vec3 b) {
+	return max(0.0, dot(a, b));
+}
 
 float positiveDot(vec4 a, vec4 b) {
 	return max(0.0, dot(a, b));
 }
 
 void main() {
-	vec4 camera = normalize(u_CameraPosition - v_VertexPosition);
-	vec4 normal = vec4(normalize((v_NormalMatrix * v_VertexNormal).xyz), 1.0);
-	vec4 reflectNormal = normalize(v_NormalMatrix * v_VertexNormal);
-	vec4 light = normalize(-u_LightDirection);
-	vec4 reflectedLight = reflect(light, reflectNormal);
+	vec3 camera = normalize(-v_VertexPosition);
+	vec3 normal = normalize((v_NormalMatrix * v_VertexNormal).xyz);
+	vec3 light = normalize((v_LightMatrix * -u_LightDirection).xyz);
+	vec3 halfVector = normalize(vec3(camera + light));
 
-	float diffuseValue = positiveDot(v_VertexNormal, light);
-	float specularValue = pow(positiveDot(reflectedLight, camera), u_Shininess) * 2.0;
+	float diffuseValue = positiveDot(normal, light);
+	float specularValue = pow(positiveDot(halfVector, normal), u_Shininess);
 
 	vec3 ambient = u_AmbientColor.rgb * 0.2;
-	vec3 diffuse = u_DiffuseColor.rgb * 0.8 * diffuseValue;
+	vec3 diffuse = u_DiffuseColor.rgb * diffuseValue * 0.8;
 	vec3 specular = u_SpecularColor.rgb * specularValue;
 
 	gl_FragColor = vec4(specular + diffuse + ambient, u_DiffuseColor.a);
