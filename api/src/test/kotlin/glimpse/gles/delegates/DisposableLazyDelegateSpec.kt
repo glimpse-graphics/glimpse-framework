2741865c03361e5ddbd75c35182342de4097f85d
Sławomir Czerwiński <slawomir@czerwinski.info.pl>
2016-10-09 17:19:33 +0200

    Added tests for disposable lazy delegate

---
+++ api/src/test/kotlin/glimpse/gles/delegates/DisposableLazyDelegateSpec.kt
@@ -0,0 +1,75 @@
+package glimpse.gles.delegates
+
+import glimpse.gles.Disposables
+import glimpse.test.GlimpseSpec
+
+class DisposableLazyDelegateSpec : GlimpseSpec() {
+
+	init {
+
+		"Disposable lazily delegated property" should {
+			"not change after initialization" {
+				var number = 1
+				class SomeClass {
+					val value : Int by DisposableLazyDelegate { number }
+				}
+				val instance = SomeClass()
+
+				instance.value shouldBe 1
+
+				forAll(integers) { integer ->
+					number = integer
+					instance.value == 1
+				}
+			}
+			"change after it was disposed" {
+				var number = 1
+				class SomeClass {
+					val value : Int by DisposableLazyDelegate { number }
+				}
+				val instance = SomeClass()
+
+				instance.value shouldBe 1
+
+				forAll(integers) { integer ->
+					Disposables.disposeAll()
+					number = integer
+					instance.value == integer
+				}
+			}
+			"not affect other disposable lazily delegated properties" {
+				var number = 1
+				class SomeClass {
+					val value1 : Int by DisposableLazyDelegate { number }
+					val value2 : Int by DisposableLazyDelegate { number }
+				}
+				val instance1 = SomeClass()
+				val instance2 = SomeClass()
+
+				instance1.value1 shouldBe 1
+
+				number = 2
+				instance1.value1 shouldBe 1
+				instance1.value2 shouldBe 2
+
+				number = 3
+				instance1.value1 shouldBe 1
+				instance1.value2 shouldBe 2
+				instance2.value1 shouldBe 3
+
+				number = 4
+				instance1.value1 shouldBe 1
+				instance1.value2 shouldBe 2
+				instance2.value1 shouldBe 3
+				instance2.value2 shouldBe 4
+
+				number = 5
+				instance1.value1 shouldBe 1
+				instance1.value2 shouldBe 2
+				instance2.value1 shouldBe 3
+				instance2.value2 shouldBe 4
+			}
+		}
+
+	}
+}
